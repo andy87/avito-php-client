ad72bf03d0eb7919e798bc8085651fc861bd5a68
Andrey and _y87 Kidin <kidin_a@list.ru>
2025-06-26 10:17:14 +0300

    update protocol info

--- src/AvitoConfig.php
+++ src/AvitoConfig.php
@@ -2,11 +2,9 @@
 
 namespace andy87\avito\client;
 
-use andy87\sdk\client\base\interfaces\ClientInterface;
-use andy87\sdk\client\helpers\Port;
+use andy87\sdk\client\helpers\Protocol;
 use andy87\avito\client\ext\AvitoAccount;
 use andy87\sdk\client\base\components\Config;
-use andy87\sdk\client\transports\CurlTransportDebug;
 
 /**
  * Configuration for Avito API client.
@@ -17,7 +15,7 @@ use andy87\sdk\client\transports\CurlTransportDebug;
  */
 class AvitoConfig extends Config
 {
-    public string $protocol = Port::HTTPS;
+    public string $protocol = Protocol::HTTPS;
 
     public string $host = 'api.avito.ru';
 }
